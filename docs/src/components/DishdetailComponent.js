d394a3f32a5d8504540263031931573394a67597
Derek <dereky1@uci.edu>
2019-02-11 18:31:25 -0800

    halfway through purifying menu component

--- docs/src/components/DishdetailComponent.js
+++ docs/src/components/DishdetailComponent.js
@@ -2,18 +2,16 @@ import React, {Component} from 'react';
 import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
 
 class DishDetail extends Component {
-
   renderComments(){
-    if (this.props.selectedDish != null){
-      var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-      const comments = this.props.selectedDish.comments.map((dish) => {
+    if (this.props.dish != null){
+      const comments = this.props.dish.comments.map((dish) => {
         return(
           <div key={dish.id} className="row">
             <div className="col-12">
               <p>{dish.comment}</p>
             </div>
             <div className="col-12">
-              <p>-- {dish.author} , {months[parseInt(dish.date.substr(5,2))-1]} {dish.date.substr(8,2)}, {dish.date.substr(0,4)}</p>
+              <p>-- {dish.author} , {new Intl.DateTimeFormat('en-US', {year:'numeric', month:'short', day:'2-digit'}).format(new Date(Date.parse(dish.date)))} </p>
             </div>
           </div>
         )
@@ -23,10 +21,10 @@ class DishDetail extends Component {
         <div className="row">
           <div className="col-12 col-md-5 m-1">
             <Card>
-              <CardImg width="100%" src={this.props.selectedDish.image} alt={this.props.selectedDish.name}/>
+              <CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name}/>
               <CardBody>
-                <CardTitle><h4>{this.props.selectedDish.name}</h4></CardTitle>
-                <CardText>{this.props.selectedDish.description}</CardText>
+                <CardTitle><h4>{this.props.dish.name}</h4></CardTitle>
+                <CardText>{this.props.dish.description}</CardText>
               </CardBody>
             </Card>
           </div>
@@ -44,7 +42,7 @@ class DishDetail extends Component {
 
   render(){
     return(
-      <div>
+      <div className="container">
         {this.renderComments()}
       </div>
     );
