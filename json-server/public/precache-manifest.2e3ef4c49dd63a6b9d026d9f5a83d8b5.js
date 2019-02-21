dd074b6cbf6c20f400aaf1e2e593776e1a898492
Derek <dereky1@uci.edu>
2019-02-21 14:51:48 -0800

    build project

---
+++ json-server/public/precache-manifest.2e3ef4c49dd63a6b9d026d9f5a83d8b5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "f6484148bec8ded23211",
+    "url": "/static/css/main.5ec28361.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "75679cb1e4cc32034ff0",
+    "url": "/static/js/1.75679cb1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f6484148bec8ded23211",
+    "url": "/static/js/main.f6484148.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "75679cb1e4cc32034ff0",
+    "url": "/static/css/1.34458774.chunk.css"
+  },
+  {
+    "revision": "92da869e6dbb98d2158bae2b46c11a2f",
+    "url": "/index.html"
+  }
+];
